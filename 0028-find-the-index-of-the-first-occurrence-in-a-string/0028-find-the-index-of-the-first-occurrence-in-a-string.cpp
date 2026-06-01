f6dd3c30d9689d5e325be50ec674f9254a2a565f
Doan Manh Hung <manhhung19960204@gmail.com>
2026-06-02 00:04:49 +0700

    Time: 0 ms (100%), Space: 9.1 MB (73.57%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-int hLen = haystack.length();
-    int nLen = needle.length();
-    
-    // Chỉ cần duyệt đến hLen - nLen vì nếu dài hơn thì không thể chứa needle được
-    for (int i = 0; i <= hLen - nLen; i++) {
-        int j = 0;
-        // Kiểm tra từng ký tự
-        for (j = 0; j < nLen; j++) {
-            if (haystack[i + j] != needle[j]) break;
-        }
-        // Nếu j chạy hết chiều dài của needle nghĩa là khớp hoàn toàn
-        if (j == nLen) return i;
-    }
-    return -1;
+        int haylengh= haystack.length();
+        int needLength = needle.length();
+        for(int i = 0 ; i <= haylengh - needLength ; i++) {
+            int j = 0;
+            for( j = 0 ; j < needLength ; j++) {
+                if(haystack[i+j] != needle[j]) break ;
+            }
 
+            if(j == needLength) return i;
+        }
+
+        return -1;
     }
 };
