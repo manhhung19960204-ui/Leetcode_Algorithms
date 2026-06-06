7111597e7ba8a8c03a5652b93ee832d265e23f03
Doan Manh Hung <manhhung19960204@gmail.com>
2026-06-07 00:45:53 +0700

    Time: 0 ms (100%), Space: 7.8 MB (86.36%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 2) return n;
+        
+            int pre2 = 1; 
+            int pre1 = 2; 
+            int current = 0;       
+        for (int i = 3; i <= n; i++) {
+            current = pre1 + pre2; 
+            pre2 = pre1;           
+            pre1 = current;
+        }
+        
+        return current;
+    }
+};
